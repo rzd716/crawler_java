0f046e1c47e19e92f5b7d6fd722461232140eae9
rzd716 <18813298636@163.com>
2016-09-11 11:17:57 +0800

    This is a test.

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
          System.out.print("this is a test");
+        System.out.print("this is another test");
+        System.out.print("this is 4st test.");
     }
 }
