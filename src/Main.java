565430e5b3953f8587eacfe780c639a369b1d879
rzd716 <18813298636@163.com>
2016-09-11 11:37:57 +0800

    This is a start of 3st branch.

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,6 @@ public class Main {
          System.out.print("this is a test");
         System.out.print("this is another test");
         System.out.print("this is 4st test.");
+        System.out.print("AAAAAAAAAA");
     }
 }
