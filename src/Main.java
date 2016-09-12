1a4963d360e8bb7fe4a4f9ea6bfb79d659762f8d
rzd716 <18813298636@163.com>
2016-09-12 22:12:20 +0800

    Fetch questions from recommendations of zhihu.

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,10 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.net.URLConnection;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.io.*;
+import java.net.*;
+import java.util.ArrayList;
+import java.util.regex.*;
 
 public class Main {
-
-    public static void main(String[] args) {
-        // 定义即将访问的链接
-        String url = "http://www.baidu.com";
-        // 访问链接并获取页面内容
-        String result = sendGet(url);
-        String imgSrc=RegexString(result, "true\\ssrc=/{2}(.+?\\.png)");
-        System.out.println(imgSrc);
-    }
-    static String sendGet(String url) {
+    static String SendGet(String url) {
         // 定义一个字符串用来存储网页内容
         String result = "";
         // 定义一个缓冲字符输入流
@@ -30,7 +19,7 @@ public class Main {
             connection.connect();
             // 初始化 BufferedReader输入流来读取URL的响应
             in = new BufferedReader(new InputStreamReader(
-                    connection.getInputStream()));
+                    connection.getInputStream(), "UTF-8"));
             // 用来临时存储抓取到的每一行的数据
             String line;
             while ((line = in.readLine()) != null) {
@@ -46,7 +35,6 @@ public class Main {
             try {
                 if (in != null) {
                     in.close();
-
                 }
             } catch (Exception e2) {
                 e2.printStackTrace();
@@ -56,14 +44,34 @@ public class Main {
 
     }
 
-    static String RegexString(String targetStr,String patternStr)
-    {
-        Pattern pattern=Pattern.compile(patternStr);
-        Matcher matcher=pattern.matcher(targetStr);
-        if (matcher.find())
-        {
-            return matcher.group(1);
+    static ArrayList<String> RegexString(String targetStr, String patternStr) {
+        // 预定义一个ArrayList来存储结果
+        ArrayList<String> results = new ArrayList<String>();
+        // 定义一个样式模板，此中使用正则表达式，括号中是要抓的内容
+        Pattern pattern = Pattern.compile(patternStr);
+        // 定义一个matcher用来做匹配
+        Matcher matcher = pattern.matcher(targetStr);
+        // 如果找到了
+        boolean isFind = matcher.find();
+        // 使用循环将句子里所有的kelvin找出并替换再将内容加到sb里
+        while (isFind) {
+            //添加成功匹配的结果
+            results.add(matcher.group(1));
+            // 继续查找下一个匹配对象
+            isFind = matcher.find();
         }
-        else return "";
+        return results;
+    }
+
+    public static void main(String[] args) {
+
+        // 定义即将访问的链接
+        String url = "http://www.zhihu.com/explore/recommendations";
+        // 访问链接并获取页面内容
+        String result = SendGet(url);
+        // 使用正则匹配图片的src内容
+        ArrayList<String> imgSrc = RegexString(result, "question_link.+?>(.+?)<");
+        // 打印结果
+        System.out.println(imgSrc);
     }
 }
